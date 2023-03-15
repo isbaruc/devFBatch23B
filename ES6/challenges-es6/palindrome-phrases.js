4d7e0b4ebe6d0a9ac6e6a7f712867cf27ad9e383
isbaruc <isbaruc@proton.me>
2023-03-14 23:13:55 -0600

    solve an unexpected case

--- ES6/challenges-es6/palindrome-phrases.js
+++ ES6/challenges-es6/palindrome-phrases.js
@@ -1,19 +1,19 @@
 // const phrase = "anita lava la tina";
 const phrase = "Anita lava la tina";
 
-const prhaseToLower = phrase.toLowerCase();
-const phraseWithoutSpaces = prhaseToLower.replaceAll(" ", "");
-const splitPrhase = phraseWithoutSpaces.split("");
-
-let noPalindromo = true;
-
 const palindrome = (prhase) => {
+	const prhaseToLower = phrase.toLowerCase();
+	const phraseWithoutSpaces = prhaseToLower.replaceAll(" ", "");
+	const splitPrhase = phraseWithoutSpaces.split("");
+
+	let noPalindromo = 0;
+
 	for (let i = splitPrhase.length; i > 0; i--) {
-		splitPrhase[splitPrhase.length - i] != splitPrhase[i - 1] ? (noPalindromo = true) : (noPalindromo = false);
-		//console.log(`Letra splitWord: ${splitPrhase[splitPrhase.length - i]} \nLetra reverseWord: ${splitPrhase[i - 1]} \nValor de i: ${i} `);
+		splitPrhase[splitPrhase.length - i] != splitPrhase[i - 1] ? noPalindromo++ : noPalindromo;
+		console.log(`Letra splitWord: ${splitPrhase[splitPrhase.length - i]} \nLetra reverseWord: ${splitPrhase[i - 1]} \nValor de i: ${i} \nnoPalindrome: ${noPalindromo}`);
 	}
 
-	if (noPalindromo === true) return console.log("Esta frase no es un palindromo");
+	if (noPalindromo > 0) return console.log("Esta frase no es un palindromo");
 	else return console.log("Esta frase si es palindromo");
 };
 
