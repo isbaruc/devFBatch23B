3d981259764658d199d3f2b37553a7573602ebb1
isbaruc <isbaruc@proton.me>
2023-03-15 13:59:45 -0600

    add support to phrases with tilde
    In addition, each step of the flow is divided into functions.

--- ES6/challenges-es6/palindrome-phrases.js
+++ ES6/challenges-es6/palindrome-phrases.js
@@ -1,20 +1,39 @@
 // const phrase = "anita lava la tina";
-const phrase = "Anita lava la tina";
+const phrase = "Alí tomó tila";
 
-const palindrome = (prhase) => {
-	const prhaseToLower = phrase.toLowerCase();
-	const phraseWithoutSpaces = prhaseToLower.replaceAll(" ", "");
-	const splitPrhase = phraseWithoutSpaces.split("");
+const normalizePhrase = (phrase) => {
+	// return console.log(prhase.normalize("NFD").replace(/([aeiou])\u0301|(u)[\u0301\u0308]/gi, "$1$2"));
+	return phrase.normalize("NFD").replace(/\u0301/gi, "");
+	/* RegExp -> https://www.tutorialspoint.com/javascript_regexp/javascript_regexp_meta_inside.htm
+	Rango acentos Unicode -> https://symbl.cc/es/unicode/blocks/combining-diacritical-marks/ */
+};
 
+const toLowerCase = (normalizedPhrase) => {
+	const phraseToLower = normalizePhrase(normalizedPhrase).toLowerCase();
+	const phraseWithoutSpaces = phraseToLower.replaceAll(" ", "");
+
+	// console.log(phraseWithoutSpaces);
+
+	return phraseWithoutSpaces;
+};
+
+const splitPhrase = (lowerPhrase) => {
+	const splitedPhrase = toLowerCase(lowerPhrase).split("");
+
+	return splitedPhrase;
+};
+
+const palindrome = (phrase) => {
 	let noPalindromo = 0;
 
-	for (let i = splitPrhase.length; i > 0; i--) {
-		splitPrhase[splitPrhase.length - i] != splitPrhase[i - 1] ? noPalindromo++ : noPalindromo;
-		console.log(`Letra splitWord: ${splitPrhase[splitPrhase.length - i]} \nLetra reverseWord: ${splitPrhase[i - 1]} \nValor de i: ${i} \nnoPalindrome: ${noPalindromo}`);
+	for (let i = splitPhrase(phrase).length; i > 0; i--) {
+		splitPhrase(phrase)[splitPhrase(phrase).length - i] != splitPhrase(phrase)[i - 1] ? noPalindromo++ : noPalindromo;
+		console.log(`Letra splitWord: ${splitPhrase(phrase)[splitPhrase(phrase).length - i]} \nLetra reverseWord: ${splitPhrase(phrase)[i - 1]} \nValor de i: ${i} \nnoPalindrome: ${noPalindromo}`);
 	}
 
 	if (noPalindromo > 0) return console.log("Esta frase no es un palindromo");
 	else return console.log("Esta frase si es palindromo");
 };
 
+normalizePhrase(phrase);
 palindrome(phrase);
