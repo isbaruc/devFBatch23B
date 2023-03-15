19aefb20a266097329bbc20b8f3e5eed5886c2dd
isbaruc <isbaruc@proton.me>
2023-03-14 23:15:30 -0600

    solve an unexpected case

--- ES6/challenges-es6/palindrome.js
+++ ES6/challenges-es6/palindrome.js
@@ -1,18 +1,18 @@
 const word = "papap";
 
-const wordToLower = word.toLowerCase();
-
-const splitWord = wordToLower.split("");
-
-let noPalindromo = true;
-
 const palindrome = (palabra) => {
+	const wordToLower = word.toLowerCase();
+
+	const splitWord = wordToLower.split("");
+
+	let noPalindromo = 0;
+
 	for (let i = splitWord.length; i > 0; i--) {
-		splitWord[splitWord.length - i] != splitWord[i - 1] ? (noPalindromo = true) : (noPalindromo = false);
+		splitWord[splitWord.length - i] != splitWord[i - 1] ? noPalindromo++ : noPalindromo;
 		// console.log(`Letra splitWord: ${splitWord[splitWord.length - i]} \nLetra reverseWord: ${splitWord[i - 1]} \nValor de i: ${i} `);
 	}
 
-	if (noPalindromo === true) return console.log("Esta palabra no es un palindromo");
+	if (noPalindromo > 0) return console.log("Esta palabra no es un palindromo");
 	else return console.log("Esta palabra si es palindromo");
 };
 
