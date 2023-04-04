79618ae09efec11232c385531cd68ad356af6d22
isbaruc <isbaruc@proton.me>
2023-04-03 21:50:51 -0600

    ejemplos y ejercicios clase

--- estructurasDatos/arrays.js
+++ estructurasDatos/arrays.js
@@ -15,6 +15,25 @@ class MyArray {
 		return this.data;
 	}
 
+	//Agrega un elemento al principio
+	unshift(item) {
+		// accedemos al ultimo elemento
+		const lastItem = this.data[this.length - 1];
+
+		// recorremos hacia adelante los valores
+		for (let i = 0; i <= this.length - 1; i++) {
+			this.data[i + 1] = this.data[i];
+		}
+
+		// actualizamos los valores del ultimo y primer elemento
+		this.data[this.length] = lastItem;
+		this.data[0] = item;
+
+		// aumentamos el this.length que es el número de elementos en nuestro array
+		this.length++;
+		return item;
+	}
+
 	//Agregar metodos pop y shift para MyArray
 }
 
